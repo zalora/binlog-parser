4baebf5b0f3b041f7d9ae8a707978a7e5d96ae5b
Max Beutel <me@maxbeutel.de>
2017-04-25 10:17:16 +0800

    SHOP-14072: add tests

--- src/zalora/binlog-parser/parse_binlog_file_integration_test.go
+++ src/zalora/binlog-parser/parse_binlog_file_integration_test.go
@@ -39,13 +39,19 @@ func TestParseBinlogFile(t *testing.T) {
 	testCases := []struct {
 		fixtureFilename  string
 		expectedJsonFile string
+		includeTables    []string
+		includeSchemas   []string
 	}{
-		{"fixtures/mysql-bin.01", "fixtures/01.json"}, // inserts and updates
-		{"fixtures/mysql-bin.02", "fixtures/02.json"}, // create table, insert
-		{"fixtures/mysql-bin.03", "fixtures/03.json"}, // insert 2 rows, update 2 rows, update 3 rows
-		{"fixtures/mysql-bin.04", "fixtures/04.json"}, // large insert (1000)
-		{"fixtures/mysql-bin.05", "fixtures/05.json"}, // DROP TABLE ... queries only
-		{"fixtures/mysql-bin.06", "fixtures/06.json"}, // table schema doesn't match anymore
+		{"fixtures/mysql-bin.01", "fixtures/01.json", nil, nil},                                  // inserts and updates
+		{"fixtures/mysql-bin.02", "fixtures/02.json", nil, nil},                                  // create table, insert
+		{"fixtures/mysql-bin.03", "fixtures/03.json", nil, nil},                                  // insert 2 rows, update 2 rows, update 3 rows
+		{"fixtures/mysql-bin.04", "fixtures/04.json", nil, nil},                                  // large insert (1000)
+		{"fixtures/mysql-bin.05", "fixtures/05.json", nil, nil},                                  // DROP TABLE ... queries only
+		{"fixtures/mysql-bin.06", "fixtures/06.json", nil, nil},                                  // table schema doesn't match anymore
+		{"fixtures/mysql-bin.01", "fixtures/01-include-table.json", []string{"buildings"}, nil},  // include tables
+		{"fixtures/mysql-bin.01", "fixtures/01-no-events.json", []string{"unknown_table"}, nil},  // only unknown table is included - no events parsed
+		{"fixtures/mysql-bin.01", "fixtures/01.json", nil, []string{"test_db"}},                  // inlcude schemas
+		{"fixtures/mysql-bin.01", "fixtures/01-no-events.json", nil, []string{"unknown_schema"}}, // only unknown schema is included - no events parsed
 	}
 
 	for _, tc := range testCases {
@@ -53,7 +59,17 @@ func TestParseBinlogFile(t *testing.T) {
 			var buffer bytes.Buffer
 			binlogFilename := filepath.Join(dataDir, tc.fixtureFilename)
 
-			err := parseBinlogFile(binlogFilename, test.TEST_DB_CONNECTION_STRING, createConsumerChain(&buffer))
+			chain := createConsumerChain(&buffer)
+
+			if tc.includeTables != nil {
+				chain.IncludeTables(tc.includeTables...)
+			}
+
+			if tc.includeSchemas != nil {
+				chain.IncludeSchemas(tc.includeSchemas...)
+			}
+
+			err := parseBinlogFile(binlogFilename, test.TEST_DB_CONNECTION_STRING, chain)
 
 			if err != nil {
 				t.Fatal(fmt.Sprintf("Expected no error when successfully parsing file %s", err))
@@ -75,6 +91,13 @@ func assertJson(t *testing.T, buffer bytes.Buffer, expectedJsonFile string) {
 	actual := strings.TrimSpace(buffer.String())
 
 	if expected != actual {
-		t.Fatal(fmt.Sprintf("JSON does not match\nExpected:\n==========\n%s\n==========\nActual generated:\n%s\n==========", expected, actual))
+		errorMessage := fmt.Sprintf(
+			"JSON file %s does not match\nExpected:\n==========\n%s\n==========\nActual generated:\n%s\n==========",
+			expectedJsonFile,
+			expected,
+			actual,
+		)
+
+		t.Fatal(errorMessage)
 	}
 }
